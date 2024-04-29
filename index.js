4077767575b6ba1ca0fa038dfdfac655bbd964ba
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-04-29 18:23:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+function lifeInWeek(age){
+   var  yarsRemaining=90-age;
+   var  days=yarsRemaining*365;
+   var  weeks=yarsRemaining*54;
+   var months=yarsRemaining*12
+   console.log("you have left,"+""+days,weeks +"and"+months+"remainings"
+)
+
+lifeInWeek(12);
+
+
+
+    
+}
