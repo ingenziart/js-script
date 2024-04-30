f978c9922a3687f02eac25b4e215a014e3874f27
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-04-30 14:36:45 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function lifeInWeek(age){
-   var  yarsRemaining=90-age;
-   var  days=yarsRemaining*365;
-   var  weeks=yarsRemaining*54;
-   var months=yarsRemaining*12
-   console.log("you have left,"+""+days,weeks +"and"+months+"remainings"
-)
-
-lifeInWeek(12);
-
-
-
-    
-}
+function getMilk(money){
+    console.log("moveRight");
+    console.log("staightForwnd");
+    console.log("turnLeft");
+    var bottlesOfMilk =Math.floor(money/1.5);
+    console.log("buy"+bottlesOfMilk+"of getMilk")
+    console.log("moveRight");
+    console.log("staightForwnd");
+    console.log("turnLeft");
+    return money%1.5
+ }
+  var change=getMilk(4)
+  console.log(change)
