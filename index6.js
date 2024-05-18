73500fdb33911f17987569a6bf13d3b9fa09e071
ingenziart <104985332+ingenziart@users.noreply.github.com>
2024-05-17 20:36:55 -0400

    Add files via upload

---
+++ index6.js
@@ -0,0 +1,38 @@
+let  x ;
+
+let  y;
+x=new Date(2024 ,8 ,12)
+x=x.toString()
+y=Date.now();
+console.log(x);
+console.log(y);
+
+//const fluit =["orange","grape","bluebery","strawbery"]
+//console.log(fluit);
+
+
+let z;
+const fluit =["orange","grape","bluebery","strawbery",45];
+z=`my favourite fluit is ${fluit[2]}`
+z=fluit.length
+fluit[2]="banana"
+z=fluit
+console.log(z)
+
+let g;
+const flui =["orange","grape","bluebery","strawbery",45];
+const boot =["peter","john","red"]
+
+flui.push(boot);
+g=flui[5][1]
+console.log(g);
+let r,
+football=['messi','januzaj','aime']
+basketball=['certics','okc','kjfb']
+
+const all =[football,basketball]
+r=all[0][0]
+r=all.flat()
+console.log(r);
+
+const ar =[1,2,3[23,34],6,[12]]
